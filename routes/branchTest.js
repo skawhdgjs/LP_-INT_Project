e60db2607b2390a74a0f15b5d64a3c11e6b09c73
sungsupark91 <wmfdkek@naver.com>
2018-02-01 14:46:32 +0900

    마스터 브랜치 branchTest.js 추가

---
+++ routes/branchTest.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//박성수 branch
+
+
+//정민우 branch
+
+
+//김율리 branch
+
+
+//김수현 branch
+
+
+
+//남종현 branch
+
+module.exports = router;
