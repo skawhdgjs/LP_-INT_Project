999aec461412aef38f64881bbff8bb267b06e850
sungsupark91 <wmfdkek@naver.com>
2018-02-01 16:14:46 +0900

    롤백
    ㅇㅇ

--- routes/branchTest.js
+++ routes/branchTest.js
@@ -1,4 +1,4 @@
-﻿var express = require('express');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
@@ -7,23 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 //박성수 branch
-<<<<<<< HEAD
-=======
 
->>>>>>> 96204e88fd411ac173da0fd8826a1cb922ddf246
-console.log("hey day!");
-/*재밌다*/
-///ㅅㄷㄴㅅㄷㄹㄷㄹ
+
 
 //정민우 branch
 
 
 
-<<<<<<< HEAD
->>>>>>> dev
-=======
-
->>>>>>> 96204e88fd411ac173da0fd8826a1cb922ddf246
 
 //김율리 branch
 
@@ -32,11 +22,8 @@ console.log("hey day!");
 //김수현 branch
 
 
-
 //남종헌 branch
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 96204e88fd411ac173da0fd8826a1cb922ddf246
+
 module.exports = router;
