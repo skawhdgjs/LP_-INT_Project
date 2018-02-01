e49a3ba824c41bb3116b8c3d143406a45bcc6130
Nam <skawhdgjs@gmail.com>
2018-02-01 15:26:29 +0900

    mod
    mod

--- routes/branchTest.js
+++ routes/branchTest.js
@@ -22,26 +22,11 @@ router.get('/minwoo', function(req, res){
 
 
 //김수현 branch
-<<<<<<< HEAD
 
 
 //남종헌 branch
-<<<<<<< HEAD
-=======
-router.get('/suhyeon',(req,res)=>{
-  res.send("kkkk");
-})
-
-
-//남종헌 branch
-
-
->>>>>>> su-hyeon-branch
-=======
 router.get('/nam',function(req ,res, next){
   res.send('hello')
 })
-
->>>>>>> 6d391e684953a185bb86e1c921976f776a4da92b
 
 module.exports = router;
