9793193ecb8b0800751d0444965c9133d8105512
sungsupark91 <wmfdkek@naver.com>
2018-02-01 15:05:49 +0900

    성수브랜치

---
+++ routes/branchTest.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//박성수 branch
+console.log("hey day!");
+/*재밌다*/
+
+//정민우 branch
+
+
+
+
+//김율리 branch
+
+
+
+//김수현 branch
+
+
+//남종헌 branch
+
+module.exports = router;
